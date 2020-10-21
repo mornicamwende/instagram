0870130dd3ff01cf572bacbff16b245838e42e48
Mornica Mwende <mornicamwende@gmail.com>
2020-10-21 20:56:41 +0300

    add class PostCreateView

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import post
 
 
@@ -8,6 +10,50 @@ def home(request):
     }
     return render(request, 'instaapp/home.html', context)
 
+class PostListView(ListView):
+    model = post
+    template_name = 'instaapp/home.html' #<app>/<model> <viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['date_posted']
+
+class PostDetailView(DetailView):
+    model = post
+   
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = post
+    fields = ['title', 'caption', 'image']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = post
+    fields = ['title', 'caption']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 def about(request):
     return render(request, 'instaapp/about.html', {'title': 'About'})
+
